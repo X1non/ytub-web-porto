9e4ebdc954a9bd19541eaefa8fa9df86c6618918
Timothy E <timothyefraim35@gmail.com>
2024-12-23 09:28:03 +0700

    remove comments, unused elements and renaming file and elements

--- static/carousel.js
+++ static/carousel.js
@@ -2,56 +2,39 @@ const el = (sel, par) => (par || document).querySelector(sel);
 const els = (sel, par) => (par || document).querySelectorAll(sel);
 const elNew = (tag, prop) => Object.assign(document.createElement(tag), prop);
 
-// Helper functions:
+const mod = (n, m) => (n % m + m) % m;
 
-const mod = (n, m) => (n % m + m) % m; // Q1: negative modulo solution, in which instance we need negative mods
-
-// Task: Carousel:
-
-const carousel = (elCarousel) => { // Q2: What's elCarousel?
+const carousel = (elCarousel) => {
 
   const animation = 500;
   const pause = 5000;
-  // Or use something like: const animation = Math.abs(elCarousel.dataset.carouselAnimation ?? 500);
 
-  // Q2b: I think its the slider, but why do we need to make a parameter of elCarousel?
   const elCarouselSlider = el(".carousel-slider", elCarousel); 
   const elsSlides = els(".carousel-slide", elCarouselSlider);
   const elsBtns = [];
 
-  let itv; // Autoslide interval
-  let tot = elsSlides.length; // Total slides
-  let c = 0; // Q3: When does c adds up?
+  let itv;
+  let tot = elsSlides.length;
+  let c = 0;
 
-  if (tot < 2) return; // Not enough slides. Do nothing.
+  if (tot < 2) return;
 
-  // Methods:
   const anim = (ms = animation) => {
     const cMod = mod(c, tot);
-    // Move slider
     elCarouselSlider.style.transitionDuration = `${ms}ms`;
     elCarouselSlider.style.transform = `translateX(${(-c - 1) * 100}%)`;
-    // Handle active classes (slide and bullet)
     elsSlides.forEach((elSlide, i) => elSlide.classList.toggle("is-active", cMod === i));
     elsBtns.forEach((elBtn, i) => elBtn.classList.toggle("is-active", cMod === i));
   };
 
-  // A3: These functions adds up c, I think is like its index of slides, which is going to be showed
   const prev = () => {
-    // A1: This is the ocassion c will reach negative numbers, when reaching index 0
-    // Q1b: But why can't we just stop on 0?
-    // A1b: Ah, maybe to help achieving infinite slides. 
-    // Using custom negative modulo, it help achieve the last index of slides
-    // Example: when c == -1, it means we've passed index 0
-    // Then, when there are 3 slides for ex (tot = 3), using custom negative mod: (-1 % 3 + 3) % 3,
-    // it will equals 2 a.k.a the alst index of slides.
-    if (c <= -1) return; // prevent blanks on fast prev-click 
+    if (c <= -1) return;
     c -= 1;
     anim();
   };
 
   const next = () => {
-    if (c >= tot) return; // prevent blanks on fast next-click
+    if (c >= tot) return;
     c += 1;
     anim();
   };
@@ -61,16 +44,13 @@ const carousel = (elCarousel) => { // Q2: What's elCarousel?
     anim();
   };
 
-  // Q4: When does play and stop triggered?
   const play = () => { 
-    itv = setInterval(next, pause + animation); // returns interval id
+    itv = setInterval(next, pause + animation);
   };
 
   const stop = () => {
-    clearInterval(itv); // stop the ongoing interval through obtained id
+    clearInterval(itv);
   };
-
-  // Buttons:
 
   const elPrev = elNew("div", {
     className: "carousel-prev",
@@ -84,13 +64,11 @@ const carousel = (elCarousel) => { // Q2: What's elCarousel?
     onclick: () => next(),
   });
 
-  // Navigation:
 
   const elNavigation = elNew("div", {
     className: "carousel-navigation",
   });
 
-  // Navigation bullets:
 
   for (let i = 0; i < tot; i++) {
     const elBtn = elNew("button", {
@@ -102,26 +80,19 @@ const carousel = (elCarousel) => { // Q2: What's elCarousel?
   }
 
 
-  // Events:
-
-  // Infinite slide effect:
   elCarouselSlider.addEventListener("transitionend", () => {
-    if (c <= -1) c = tot - 1; // Hint 1: sets index from 0 to last slides
-    if (c >= tot) c = 0; // Hint 2: sets index from last slides to the first one
-    anim(0); // quickly switch to "c" slide (with animation duration 0)
+    if (c <= -1) c = tot - 1; 
+    if (c >= tot) c = 0; 
+    anim(0); 
   });
 
-  // Pause on pointer enter:
-  elCarousel.addEventListener("pointerenter", () => stop()); // A4: Here is the event which makes slides play/stop
+
+  elCarousel.addEventListener("pointerenter", () => stop());
   elCarousel.addEventListener("pointerleave", () => play());
 
-  // Init:
-
-  // Insert UI elements:
   elNavigation.append(...elsBtns);
   elCarousel.append(elPrev, elNext, elNavigation);
 
-  // Clone first and last slides (for "infinite" slider effect)
   elCarouselSlider.prepend(elsSlides[tot - 1].cloneNode(true));
   elCarouselSlider.append(elsSlides[0].cloneNode(true));
 
@@ -132,6 +103,4 @@ const carousel = (elCarousel) => { // Q2: What's elCarousel?
   play();
 };
 
-// A2: Maybe this. For each implemented carousel, add the infinetly slides carousel function.
-// Allows to use multiple carousels on the same page:
 els(".carousel").forEach(carousel);
--- static/nav.js
+++ static/nav.js
@@ -1,4 +1,3 @@
-// add classes for mobile navigation toggling
 var CSbody = document.querySelector('body');
 const CSnavbarMenu = document.querySelector('#cs-navigation');
 const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
@@ -7,11 +6,9 @@ CShamburgerMenu.addEventListener('click', function () {
 	CShamburgerMenu.classList.toggle('cs-active');
 	CSnavbarMenu.classList.toggle('cs-active');
 	CSbody.classList.toggle('cs-open');
-	// run the function to check the aria-expanded value
 	ariaExpanded();
 });
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
 function ariaExpanded() {
 	const csUL = document.querySelector('#cs-expanded');
 	const csExpanded = csUL.getAttribute('aria-expanded');
@@ -23,7 +20,6 @@ function ariaExpanded() {
 	}
 }
 
-// mobile nav toggle code
 const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
 for (const item of dropDowns) {
 	const onClick = () => {
